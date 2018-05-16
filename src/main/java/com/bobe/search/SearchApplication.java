7a3aaa43f03e42f6446634cf7897a157aa076659
ccbobe <1132717195@qq.com>
2018-05-16 23:19:58 +0800

    添加新的配置文件及其依赖版本信息

--- src/main/java/com/bobe/search/SearchApplication.java
+++ src/main/java/com/bobe/search/SearchApplication.java
@@ -1,7 +1,20 @@
 package com.bobe.search;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAutoConfiguration
+@EnableMBeanExport
+@EnableScheduling
+@EnableAsync
+@EnableCaching
 @SpringBootApplication
 public class SearchApplication {
 
